bcc2b864a1c086334f543424a01ef1851ee68567
fallais <francois.allais@sogeti.com>
2016-09-29 17:26:51 +0200

    Improve

--- main.go
+++ main.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-	"flag"
 	"net"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/zenazn/goji"
 )
 
 func main() {
 	//Connect udp
-	conn, err := net.Dial("udp", "192.168.7.10:514")
+	serverAddr, err := net.ResolveUDPAddr("udp", "192.168.7.10:514")
+	localAddr, err := net.ResolveUDPAddr("udp", "192.168.100.10")
+	conn, err := net.DialUDP("udp", localAddr, serverAddr)
 	if err != nil {
 		logrus.Fatal("Error while parsing the logs", err)
 	}
 	defer conn.Close()
 
-	//simple Read
-	buffer := make([]byte, 1024)
-	conn.Read(buffer)
-
 	//simple write
-	conn.Write([]byte("Sep 18 10:28:52 server 2013-09-18T10:28:52Z server [programname][20310]: Log message "))
-
-	flag.Set("bind", ":8080")
-	goji.Serve()
+	conn.Write([]byte("Sep 18 10:28:52 orchestrator [programname][20310]: Log message "))
+	conn.Write([]byte("Sep 18 10:28:52 192.168.100.10 [programname][20310]: Log message "))
 }
